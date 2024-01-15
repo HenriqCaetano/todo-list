1e558803327f033ae5f393d6c7a35bae80d8af1c
henrique <hecaetano1@hotmail.com>
2024-01-15 12:22:40 -0300

    :sparkles: adds completed checkbox

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,16 +2,15 @@ import React from "react";
 export function TodoList({ todoList, handleDelete }) {
   return (
     <div>
-      <li>
         {todoList.map((todo) => {
           return (
             <ul key={todo.id}>
+              <input type="checkbox" />
               <span>{todo.name}</span>
-              <button onClick={handleDelete}>Delete todo</button>
+              <button onClick={handleDelete}> Delete todo</button>
             </ul>
           );
         })}
-      </li>
     </div>
   );
 }
